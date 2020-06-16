9fc85ac5de1423441c66b0ddaac364634e413ad2
Alexander Yampolski <al.yampolski@gmail.com>
2020-06-16 18:54:46 +0300

    Set header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { INavLinks } from '../../models/navLinks.model';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  mainLinks: INavLinks[] = [{ name: 'Loans', value: 'loans' }];
+  secondaryLinks: INavLinks[] = [
+    { name: 'My profile', value: 'myProfile', icon: '../../../assets/imgs/profile.svg' },
+    { name: 'Log out', value: 'LogOut', icon: '../../../assets/imgs/logout.svg' },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/models/navLinks.model.ts
@@ -0,0 +1,5 @@
+export interface INavLinks {
+    name: string;
+    value: string;
+    icon?: string;
+}
